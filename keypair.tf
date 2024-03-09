76accb1a2e860b958540a086769627f2f71fbe58
Ayman Osman <95719911+AymanAhmedOsman@users.noreply.github.com>
2024-03-09 06:56:24 +0200

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,13 +1,13 @@
 #-------------------------------- private key ssh ---------------------
-#resource "tls_private_key" "ABI_key" {
-#  algorithm = "RSA"
-#  rsa_bits  = 4096
-#}
+resource "tls_private_key" "ABI_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-#resource "aws_key_pair" "ABI_ssh_key" {
- # key_name   = "API_test_key"
- # public_key = tls_private_key.ABI_key.public_key_openssh
+resource "aws_key_pair" "ABI_ssh_key" {
+  key_name   = "API_test_key"
+  public_key = tls_private_key.ABI_key.public_key_openssh
   
   
-#}
+}
 
