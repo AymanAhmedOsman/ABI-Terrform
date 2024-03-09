5e60bfafebeb06233049e5d8e021462f7f9fcc7a
Ayman Osman <95719911+AymanAhmedOsman@users.noreply.github.com>
2024-03-09 06:57:01 +0200

    Update secretmanager.tf

--- secretmanager.tf
+++ secretmanager.tf
@@ -1,12 +1,12 @@
 #------------------------------------ Secret Manager ---------------------
 
-#resource "aws_secretsmanager_secret" "ABI_ssh_private_key_secret" {
-#  name = "API_ssh_private_key_secret"
-#  recovery_window_in_days = 0
-#}
+resource "aws_secretsmanager_secret" "ABI_ssh_private_key_secret" {
+  name = "API_ssh_private_key_secret"
+  recovery_window_in_days = 0
+}
 
-#resource "aws_secretsmanager_secret_version" "API_ssh_private_key_secret_version" {
-# secret_id = aws_secretsmanager_secret.ABI_ssh_private_key_secret.id
-#  secret_string = tls_private_key.ABI_key.public_key_pem 
+resource "aws_secretsmanager_secret_version" "API_ssh_private_key_secret_version" {
+ secret_id = aws_secretsmanager_secret.ABI_ssh_private_key_secret.id
+ secret_string = tls_private_key.ABI_key.public_key_pem 
    
 #}
