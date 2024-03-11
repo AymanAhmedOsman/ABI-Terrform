5d3967e8cb53fcca205e0b8401632acc0380a7b0
Ayman Osman <95719911+AymanAhmedOsman@users.noreply.github.com>
2024-03-11 11:35:14 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,17 +8,6 @@ resource "aws_instance" "Front-end" {
     key_name = aws_key_pair.ABI_ssh_key.key_name
     
     associate_public_ip_address = true
-    user_data = <<-EOF
-  
-    #!/bin/bash
-# Use this for your user data (script from top to bottom)
-# install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-
-      EOF
 
 
 }
